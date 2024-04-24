e72a32d1c92b27a973041e60f8a1681af6b80dea
Epeny14 <epenyblaise@yahoo.fr>
2024-04-25 01:06:52 +0200

    troisième commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,32 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
+/*========== Menu SHOW =========*/
+/* Validate if constant exists */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*========== Menu Hidden =========*/
+/* Validate if constant exists */
+
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const bgHeader = () => {
+    const header = document.getElementById('header')
+    //When the scroll is greater than 50 viewport height, add the
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+        : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', bgHeader)
 
-
-/*=============== DARK LIGHT THEME ===============*/ 
+/*=============== DARK LIGHT THEME ===============*/
